598a2d07312fa6cd211cea05bb33c315c231ba84
mayi <mayi@keruyun.com>
2018-04-01 22:54:13 +0800

    增加profile

--- src/main/java/cn/my/orange/user/service/UserService.java
+++ src/main/java/cn/my/orange/user/service/UserService.java
@@ -4,5 +4,10 @@ import cn.my.orange.user.entity.User;
 
 public interface UserService {
 
+    /**
+     * 根据id获取用户
+     * @param id 用户id
+     * @return 用户信息
+     */
     User getById(Long id);
 }
